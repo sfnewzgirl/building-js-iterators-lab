0b7ebb1495f943a0be8911fa2448eab284e29944
YOUR_GITHUB_USERNAME <YOUR_GITHUB_EMAIL_ADDRESS>
2016-08-23 21:44:26 -0700

    creates myReduce

--- starter-code/myReduce.js
+++ starter-code/myReduce.js
@@ -1,15 +1,21 @@
 // See https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/Reduce for more details
 // Don't worry about initialValue at first. You can always add it in later.
 
-function myReduce(arr, callback) {
-  var newArray = [];
-  for (var i = 0, i < arr.length; i++) {
-
-    (arr[i], i, arr)
+function myReduce(arr, callback, initialValue) {
+  var initialIndex;
+  var returnValue;
+  if (initialValue === undefined) {
+    initialIndex = 1;
+    returnValue = arr[0];
   }
-
-//  CODE INSIDE HERE   //
-
+  else {
+    initialIndex = 0;
+    returnValue = initialValue;
+  }
+  for (var i = initialIndex; i < arr.length; i++) {
+    returnValue = callback(returnValue, arr[i], i, arr);
+  }
+  return returnValue;
 }
 
 
